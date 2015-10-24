769a5a8bcfcbf669a38993516d57d9d7a9a5a344
Yasser Gonzalez <yasserglez@gmail.com>
2015-10-23 20:25:07 -0400

    Solution to the bigger-is-greater HackerRank problem

---
+++ hackerrank/bigger-is-greater.py
@@ -0,0 +1,31 @@
+# https://www.hackerrank.com/challenges/bigger-is-greater
+
+import fileinput
+
+
+def bigger_is_greater(word):
+    word = list(word)
+    min_gt = '{'  # greater than 'z'
+    i = len(word) - 2
+    while i >= 0:
+        for j in range(i + 1, len(word)):
+            if word[i] < word[j]:
+                min_gt = min(word[j], min_gt)
+                min_gt_index = j
+        if min_gt != '{':
+            break
+        i -= 1
+    if min_gt == '{':
+        return 'no answer'
+    else:
+        word[i], word[min_gt_index] = word[min_gt_index], word[i]
+        word[i + 1:] = sorted(word[i + 1:])
+        return ''.join(word)
+
+
+if __name__ == '__main__':
+    with fileinput.input() as fi:
+        T = int(fi.readline())
+        for t in range(T):
+            word = fi.readline().rstrip()
+            print(bigger_is_greater(word))
