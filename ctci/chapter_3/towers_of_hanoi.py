5d18e1e68472ac48eed0c674251f61234f9c629e
Yasser Gonzalez <yasserglez@gmail.com>
2015-11-02 09:37:09 -0500

    Update problem 3.4 from CtCI

--- ctci/chapter_3/towers_of_hanoi.py
+++ ctci/chapter_3/towers_of_hanoi.py
@@ -1,31 +1,30 @@
 # Interview Question 3.4
 
-
-def recursive_solution(num_discs, tower1, tower2, tower3):
-    _move(num_discs, tower1, tower3, tower2)
+from collections import namedtuple
 
 
-def _move(num_discs, orig, dest, aux):
+Tower = namedtuple('Tower', ['name', 'disks'])
+
+
+def solve(orig, aux, dest):
+    num_discs = len(orig.disks)
+    _move(num_discs, orig, aux, dest)
+
+
+def _move(num_discs, orig, aux, dest):
     if num_discs > 0:
-        _move(num_discs - 1, orig, aux, dest)
-        print('move one disk from {0} to {1}'.format(orig, dest))
-        _move(num_discs - 1, aux, dest, orig)
-
-
-def iterative_solution(num_discs, tower1, tower2, tower3):
-    stack = []
-    stack.append((num_discs, tower1, tower3, tower2))
-    while stack:
-        item = stack.pop()
-        if len(item) == 2:
-            print('move one disk from {0} to {1}'.format(*item))
-        else:
-            k, orig, dest, aux = item
-            if k > 0:
-                stack.append((k - 1, aux, dest, orig))
-                stack.append((orig, dest))
-                stack.append((k - 1, orig, aux, dest))
+        _move(num_discs - 1, orig, dest, aux)
+        dest.disks.append(orig.disks.pop())
+        if len(dest.disks) > 1:
+            assert dest.disks[-1] < dest.disks[-2]
+        print('move one disk from {0.name} to {1.name}'.format(orig, dest))
+        _move(num_discs - 1, aux, orig, dest)
 
 
 if __name__ == '__main__':
-    iterative_solution(5, 'A', 'B', 'C')
+    tower_a = Tower(name='A', disks=list(range(3, 0, -1)))
+    tower_b = Tower(name='B', disks=[])
+    tower_c = Tower(name='C', disks=[])
+    print(tower_a.disks, tower_b.disks, tower_c.disks)
+    solve(tower_a, tower_b, tower_c)
+    print(tower_a.disks, tower_b.disks, tower_c.disks)
