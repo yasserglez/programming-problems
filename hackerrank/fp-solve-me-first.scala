8a3a2db4fa7b811f335620b78ecd1d1fd4a035df
Yasser Gonzalez <yasserglez@gmail.com>
2015-11-15 18:33:10 -0500

    Solution to the fp-solve-me-first HackerRank problem

---
+++ hackerrank/fp-solve-me-first.scala
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/fp-solve-me-first
+
+object Solution {
+  def main(args: Array[String]) = {
+    println(io.Source.stdin.getLines().take(2).map(_.toInt).sum)
+  }
+}
