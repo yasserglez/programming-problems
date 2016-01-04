88f41fa16efc9dc38262451f45d765e52cf0a8c1
Yasser Gonzalez <yasserglez@gmail.com>
2016-01-03 19:16:10 -0500

    Solution to the fp-list-replication HackerRank problem

---
+++ hackerrank/functional-programming/fp-list-replication.scala
@@ -0,0 +1,14 @@
+// https://www.hackerrank.com/challenges/fp-list-replication
+
+object Solution {
+  def f(num: Int, arr: List[Int]): List[Int] = {
+    arr.flatMap(x => List.fill(num)(x))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = io.Source.stdin.getLines()
+    val num = input.take(1).map(_.toInt).next
+    var arr = input.map(_.toInt).toList
+    f(num, arr).foreach(println)
+  }
+}
