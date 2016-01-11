4c48d1667f93d7e5ebb1f49d7756a9074a2ec009
Yasser Gonzalez <yasserglez@gmail.com>
2016-01-10 20:40:25 -0500

    Solution to the fp-array-of-n-elements HackerRank problem

---
+++ hackerrank/functional-programming/fp-array-of-n-elements.scala
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/fp-array-of-n-elements
+
+object Solution {
+  def f(num: Int): List[Int] = (1 to num).toList
+
+  def main(args: Array[String]): Unit = {
+    val input = io.Source.stdin.getLines()
+    val num = input.take(1).map(_.toInt).next
+    f(num).foreach(println)
+  }
+}
