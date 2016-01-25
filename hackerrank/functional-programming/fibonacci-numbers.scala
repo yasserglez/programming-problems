8dad30cc710d0904f6f0391f3a43820d44e6830b
Yasser Gonzalez <yasserglez@gmail.com>
2016-01-24 23:49:54 -0500

    Solution to the fibonacci-numbers HackerRank problem

---
+++ hackerrank/functional-programming/fibonacci-numbers.scala
@@ -0,0 +1,15 @@
+// http://www.hackerrank.com/challenges/functional-programming-warmups-in-recursion---fibonacci-numbers
+
+object Solution {
+
+  val numbers: Stream[Int] = 0 #:: 1 #:: (numbers zip numbers.tail).map(l => l._1 + l._2)
+
+  def fibonacci(n: Int): Int = {
+    numbers.drop(n - 1).head
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lines = io.Source.stdin.getLines()
+    println(fibonacci(lines.next.toInt))
+  }
+}
