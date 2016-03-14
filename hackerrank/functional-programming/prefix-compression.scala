02941c33ec1b13b68b8069030b55156aae436404
Yasser Gonzalez <yasserglez@gmail.com>
2016-03-13 23:43:42 -0400

    Solution to the prefix-compression HackerRank problem

---
+++ hackerrank/functional-programming/prefix-compression.scala
@@ -0,0 +1,16 @@
+object Solution {
+
+  def prefixCompression(x: String, y: String, p: String = ""): List[String] = {
+    if (x.isEmpty || y.isEmpty || x.head != y.head) {
+      List(p, x, y)
+    } else {
+      prefixCompression(x.tail, y.tail, p + x.head)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lines = io.Source.stdin.getLines()
+    val result = prefixCompression(lines.next, lines.next)
+    result foreach (s => println(s"${s.length} $s"))
+  }
+}
